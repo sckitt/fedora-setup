24deac2678ae1f4a7232ae6006f8543a4b2ce943
sckitt <64489760@keemail.me>
2020-07-24 09:12:23 -0700

    updated packages

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
 # fedora-setup
-Setup Script for Fedora
-
-Video Walkthrough
-https://www.youtube.com/watch?v=400g2uVV090
+Setup Script for Termux
 
 Use the following command to run the script easily in one run:
 
-wget https://raw.githubusercontent.com/ChrisTitusTech/fedora-setup/master/fedora-setup.sh && sudo chmod +x fedora-setup.sh && ./fedora-setup.sh
+wget https://raw.githubusercontent.com/sckitt/termux-setup/master/termux-setup.sh && sudo chmod +x termux-setup.sh && ./termux-setup.sh
 
-Below is everything is included in this script:
+Below is everything included in this script:
 
-1. Install workstation repositories (google, rpmfusion, atom)
-2. Install Dropbox and Teamviewer via rpm download
-3. Install packages via DNF (This is 44 package suites)
-**See complete listing @ https://github.com/ChrisTitusTech/fedora-setup/blob/master/fedora.packages
-4. Edit bashrc to make a smarter terminal
-5. Disable Wayland and use Xorg for Graphics
+1. Install workstation repositories (rpmfusion)
+2. Install packages via rpm download (balenaEtcher, Tixati)
+3. Download packages via wget (Tutanota, Tor)
+4. Install packages via custom repo (Sublime-Text, VSCodium, youtube-dl)
+5. Install packages via DNF (25+ package suites)
+**See complete listing @ https://github.com/sckitt/fedora-setup/blob/master/fedora.packages
+6. Install [inject-hatred](https://github.com/sckitt/inject-hatred)
+7. Edit bashrc to make a beautiful terminal
+8. Install custom Proton builds
+
+Below is a list of optional/manual installs:
+
+* improving gaming performance ([video walkthrough](https://youtu.be/-jLCjY7PNig?t=466))
+    * [DXVK](https://github.com/lutris/docs/blob/master/HowToDXVK.md)
+    * [Wine dependencies](https://github.com/lutris/docs/blob/master/WineDependencies.md)
+    * [driver support](https://github.com/lutris/docs/blob/master/InstallingDrivers.md)
+    * [graphics drivers](https://github.com/doitsujin/dxvk/wiki/Driver-support)
+* [projectM](https://github.com/projectM-visualizer/projectM)
--- fedora-setup.sh
+++ fedora-setup.sh
@@ -2,26 +2,46 @@
 cd ~
 # First we setup the repositories so we can install lots of stuff
 # Set Virtual Box repo
-wget https://raw.githubusercontent.com/ChrisTitusTech/fedora-setup/master/fedora.packages
-wget https://raw.githubusercontent.com/ChrisTitusTech/fedora-setup/master/bashrc.aliases
-# Install Google Chrome repo
-sudo dnf install fedora-workstation-repositories -y
+wget https://raw.githubusercontent.com/sckitt/fedora-setup/master/fedora.packages
 # Install Rpmfusion repo
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-# Install Atom repo
-sudo rpm --import https://packagecloud.io/AtomEditor/atom/gpgkey
-sudo sh -c 'echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/atom/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/AtomEditor/atom/gpgkey" > /etc/yum.repos.d/atom.repo'
+sudo dnf install rpmfusion-free-release-tainted https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+# Install balenaEtcher
+cd ~/Downloads
+curl -s -v https://api.github.com/repos/balena-io/etcher/releases/latest | grep "browser_download_url.*x86_64.rpm" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+sudo rpm -ivh balena*
+rm balena*
+# Install Tixati
+sudo dnf install https://download2.tixati.com/download/tixati-2.73-1.x86_64.rpm -y
+# Download Tutanota
+wget https://mail.tutanota.com/desktop/tutanota-desktop-linux.AppImage
+# Download Tor
+wget https://www.torproject.org/dist/torbrowser/9.0.10/tor-browser-linux64-9.0.10_en-US.tar.xz
+tar xf tor-browser-linux64-9.0.10_en-US.tar.xz
+rm tor-browser-linux64-9.0.10_en-US.tar.xz
+cd ~
+# Install Sublime-Text
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+# Install VSCodium
+sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg 
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
+# Install youtube-dl
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
 # Update cache for package installs
 sudo dnf makecache
-# Install Dropbox
-sudo dnf install https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm -y
-# Install TeamViewer
-sudo dnf install https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm -y
 # grab all packages to install
 sudo dnf install $(cat ~/fedora.packages) -y
 
-#Custom shell prompt with aliases Source: https://www.linuxquestions.org/questions/linux-general-1/ultimate-prompt-and-bashrc-file-4175518169/
-cat ~/bashrc.aliases >> ~/.bashrc
+# Install inject-hatred
+git clone https://github.com/sckitt/inject-hatred.git && cd inject-hatred && sudo chmod +x inject-hatred.sh
 
-#Disable Wayland and use Xorg
-sudo sed -i '/WaylandEnable/s/^#//g' /etc/gdm/custom.conf
+# Shell beautification Source: https://github.com/ChrisTitusTech/scripts/blob/master/fancy-bash-promt.sh
+cd ~
+cat ~/fancy-bash-promt.sh >> ~/.bashrc
+
+# Install custom Proton builds
+curl -s -v https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep "browser_download_url.*tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/4.21-GE-2/Proton-4.21-GE-2.tar.gz
+mkdir ~/.steam/steam/compatibilitytools.d
+for i in *.gz; do tar -xvzf "$i"  -C ~/.steam/steam/compatibilitytools.d; done
